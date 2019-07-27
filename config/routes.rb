f7de11adf91c57223f148049b07a03f106b3e6ab
gadget2013 <niccolo1469@tutanota.com>
2019-07-26 19:41:06 -0700

    first sample blogger commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+=begin
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+=end
+
+Blogger::Application.routes.draw do
+  resources :articles
+end
