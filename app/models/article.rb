05663966b592bc1a6cdcee5283bd3d6e6150dc6c
gadget2013 <niccolo1469@tutanota.com>
2019-07-27 22:11:01 -0700

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,16 @@
-=begin
 class Article < ApplicationRecord
-end
-=end
-class Article < ActiveRecord::Base
-    has_many :comments
+
+    has_many :comments, dependent: :destroy
+    has_many :taggings, dependent: :destroy
+    has_many :tags, through: :taggings, dependent: :destroy
+
+    def tag_list
+        tags.join(", ")
+    end
+    
+    def tag_list=(tags_string)
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+        new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+        self.tags = new_or_found_tags
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Blogger::Application.routes.draw do
   resources :articles do 
     resources :comments
   end
+  resources :tags
+
 end
